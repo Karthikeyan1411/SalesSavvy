d746141f66ab5726fc4fadc8ef0022f26205cf81
Karthikeyan1411 <karthikeyan1411m@gmail.com>
2025-12-01 19:40:02 +0530

    Updating latest version files

--- SalesSavvy-Backend/src/main/java/com/example/demo/repositories/JWTTokenRepository.java
+++ SalesSavvy-Backend/src/main/java/com/example/demo/repositories/JWTTokenRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,6 @@ public interface JWTTokenRepository extends JpaRepository<JWTToken, Integer>{
 	
 	@Query("SELECT t FROM JWTToken t WHERE t.user.userId = :userId")
 	JWTToken findByUserId(@Param("userId") int userId);
+
+	Optional<JWTToken> findByToken(String token);
 }
